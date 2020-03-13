df7662ebad9351a3853f46d0a95535c0bd99e4cd
53554 <53554@etu.he2b.be>
2020-03-13 19:30:51 +0100

    Humbug Game Under MVC This commit announced the begin of humbug code under MVC contain only the SquareType enum

---
+++ src/main/java/g53554/humbug/model/SquareType.java
@@ -0,0 +1,12 @@
+package g53554.humbug.model;
+
+/**
+ * SquareType represents the type of a square on the board.
+ * Square are grass or star when there represent arrival.
+ *
+ * @author jj
+ */
+public enum SquareType {
+    GRASS, STAR;
+
+}
