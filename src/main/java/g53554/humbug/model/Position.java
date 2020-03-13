3f7e34da4c83f17ee9a8e074315d321d96162d28
53554 <53554@etu.he2b.be>
2020-03-13 20:20:20 +0100

    Class Position added This class will determine where we are on the board It contains two attributs row and column that are final with getter and constructor for Position It also contains equals and hashcode

---
+++ src/main/java/g53554/humbug/model/Position.java
@@ -0,0 +1,81 @@
+package g53554.humbug.model;
+
+/**
+ * Position on the board it help us to how 
+ * where we are on the board
+ *
+ * @author jj
+ */
+public class Position {
+
+    private final int row;
+    private final int column;
+
+    /**
+     * Constructor of Position with row 
+     * and column as argument
+     *
+     * @param row
+     * @param column
+     */
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    /**
+     * Getter for column
+     *
+     * @return column
+     */
+    public int getColumn() {
+        return column;
+    }
+
+    /**
+     * Getter for row
+     *
+     * @return row
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * hashCode for position
+     *
+     * @return hash
+     */
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 19 * hash + this.row;
+        hash = 19 * hash + this.column;
+        return hash;
+    }
+
+    /**
+     * Equals method for position
+     *
+     * @param obj
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Position other = (Position) obj;
+        if (this.row != other.row) {
+            return false;
+        }
+        return this.column == other.column;
+    }
+
+}
