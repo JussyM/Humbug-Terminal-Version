43021fed1cbf23473d294b263e895d5294a08489
53554 <53554@etu.he2b.be>
2020-03-13 19:38:52 +0100

    New Class Square here is the square of the board and it has as an attribut a SquareType The constructor and the a simple getter for type is added

---
+++ src/main/java/g53554/humbug/model/Square.java
@@ -0,0 +1,31 @@
+package g53554.humbug.model;
+
+/**
+ * Square on the board. A square has a type grass or star and it’s all. A square
+ * doesn’t know where it is on the board.
+ *
+ * @author jj
+ */
+public class Square {
+
+    private SquareType type;
+
+    /**
+     * Constructor of the square on the board
+     *
+     * @param type
+     */
+    public Square(SquareType type) {
+        this.type = type;
+    }
+
+    /**
+     * Getter of the type
+     *
+     * @return type
+     */
+    public SquareType getType() {
+        return type;
+    }
+
+}
